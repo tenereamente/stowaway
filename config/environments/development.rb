59fa37547de3204d6a3781ef936ad697a79adf6e
Elliot Murphy <elliot.murphy@gmail.com>
2013-08-27 23:08:44 -0400

    Make specs pass.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,15 +25,13 @@ StowAway::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true  
+  config.assets.debug = true
 
-    config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 25,
-      :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_API_KEY"]
-    }
+  config.action_mailer.delivery_method = :letter_opener
+
   # Send email in development mode.
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options ||= { host: 'localhost', port: 3000 }
 
 end
