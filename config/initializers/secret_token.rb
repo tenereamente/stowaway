f1059802c5f917dc07998176dddd2c0f96c23515
Elliot Murphy <elliot.murphy@gmail.com>
2013-08-27 22:38:30 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StowAway::Application.config.secret_key_base = '9d35df391e02b22c0b15faae42c158720facec525ccd71bebb32c7c634a658dea08c5d2b4620db3b2fe018bc64cb33246a5b5af9e564a8c2edcaaf1da2a49ca3'
