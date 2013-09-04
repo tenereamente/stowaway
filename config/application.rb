06be94280c70e8de5b821eecffccc3999a15edd7
Elliot Murphy <elliot.murphy@gmail.com>
2013-09-03 22:48:42 -0400

    Don't initialize on asset precompile

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module StowAway
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.initialize_on_precompile = false
     config.assets.enabled = true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ StowAway::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.assets.initialize_on_precompile = false
+
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
