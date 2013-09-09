0f06b703b46bf2ad13872a1d543cf59bda262670
Elliot Murphy <elliot.murphy@gmail.com>
2013-09-09 18:00:50 -0400

    Prelaunch layout for non-logged in users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # select pre-launch layout if nobody logged in
+  layout :layout
+
+  def layout
+    if user_signed_in?
+      "application"
+    else
+      "prelaunchlayout"
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
