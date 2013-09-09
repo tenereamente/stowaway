e13d33acab4c0b078f9da10d112332e272d1e11c
Elliot Murphy <elliot.murphy@gmail.com>
2013-09-09 18:24:41 -0400

    Narrow scope of prelaunch layout.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    # select pre-launch layout if nobody logged in
+    if user_signed_in?
+      render :layout => "application"
+    else
+      render :layout => "prelaunchlayout"
+    end
   end
 end
