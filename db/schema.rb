9b043fb6a2210f22201d2c7e1fcfc66988469941
Elliot Murphy <elliot.murphy@gmail.com>
2013-09-13 10:35:49 -0400

    First steps of facebook auth.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910122523) do
+ActiveRecord::Schema.define(version: 20130913040347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 20130910122523) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.string   "zip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
