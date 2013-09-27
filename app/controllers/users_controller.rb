f7372dce71c1f41e37a1efa023ff22032ed02d23
Elliot Murphy <elliot.murphy@gmail.com>
2013-09-27 08:11:29 -0400

    Fix user ordering.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all.order('created_at ASC')
+    @users = User.all.order(:created_at).reverse_order
   end
 
   def show
