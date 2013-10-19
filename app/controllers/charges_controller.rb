8a79f0f2eff70c6a9d9055587a6ad404b94389dd
Elliot Murphy <elliot.murphy@gmail.com>
2013-10-19 18:14:17 -0400

    Next phase of user billing.
    Spaces are marked as available or not.
    Get user to sign in before booking.
    Process credit card token and add invoice charge - needs debugging.
    
    Still remaining:
    - show spaces I have booked and let me cancel
    - notify space owner the space was booked
    - notify the site admins a space was booked?
    - more testing of credit card handling of charge declined

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,33 +7,61 @@ class ChargesController < ApplicationController
   def create
     @space = Space.find(params[:space_id])
     @amount = @space.monthly_price * 100 # price is stored in dollars, convert to cents
-    
-    plan = Stripe::Plan.retrieve("#{@space.monthly_price}_monthly")
-    # TODO: try-except to detect when the plan doesn't exist and create a snowflake plan on the fly
+    # exception catching is disabled here because we don't have extensive error handling
+    # protocols defined yet. It is better to not catch the exceptions and let them go
+    # up the stack to be logged in sentry rather than catching and ignoring them here.
+    begin
+      plan = Stripe::Plan.retrieve("#{@space.monthly_price}_monthly")
+      # TODO: catch the exception when the plan doesn't exist and create a snowflake plan on the fly?
 
-    # TODO: check to see if the current user already has a customer ID, if so, use that one rather
-    # than creating a new one.
+      # TODO: consider moving to stripe connect to enable more of a marketplace model where the
+      # stripe customer is on our top-level account and can purchase from multiple providers.
+      # Currently a customer can only rent a single space.
 
-    # TODO: consider moving to stripe connect to enable more of a marketplace model where the
-    # stripe customer is on our top-level account and can purchase from multiple providers.
-    # Currently a customer can only rent a single space.
+      # TODO: debug customer creation
+      customer = Stripe::Customer.create(
+        email: current_user.email, 
+        card:  params[:stripeToken],
+        plan:  plan
+      )
 
-    customer = Stripe::Customer.create(
-      email: current_user.email, 
-      card:  params[:stripeToken],
-      plan:  plan
-    )
+      # save customer ID in the user record, mark the space as booked
+      current_user.update_attribute(:stripe_customer_id, customer.id)
+      @space.update_attribute(:available, false)
 
-    # TODO add last month rent as a deposit on the invoice
+      # add last month rent as a deposit on the invoice
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => "Stowaway.co last months rent for #{@space.normalized_address}",
+        :currency    => 'usd'
+      )
+    #rescue Stripe::CardError => e
+      # Since it's a decline, Stripe::CardError will be caught
+      #body = e.json_body
+      #err  = body[:error]
 
-    # TODO save customer ID in the user record, mark the space as booked
+      redirect_to space_path(@space), alert: 'Congratulations, you have booked this space'
 
-    #charge = Stripe::Charge.create(
-    #  :customer    => customer.id,
-    #  :amount      => @amount,
-    #  :description => 'Rails Stripe customer', # TODO
-    #  :currency    => 'usd'
-    #)
+      #puts "Status is: #{e.http_status}"
+      #puts "Type is: #{err[:type]}"
+      #puts "Code is: #{err[:code]}"
+      # param is '' in this case
+      #puts "Param is: #{err[:param]}"
+      #puts "Message is: #{err[:message]}"
+    #rescue Stripe::InvalidRequestError => e
+      # Invalid parameters were supplied to Stripe's API
+    #rescue Stripe::AuthenticationError => e
+      # Authentication with Stripe's API failed
+      # (maybe you changed API keys recently)
+    #rescue Stripe::APIConnectionError => e
+      # Network communication with Stripe failed
+    #rescue Stripe::StripeError => e
+      # Display a very generic error to the user, and maybe send
+      # yourself an email
+    #rescue => e
+    # Something else happened, completely unrelated to Stripe
+    #end
 
     rescue Stripe::CardError => e
     flash[:error] = e.message
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011201504) do
+ActiveRecord::Schema.define(version: 20131019213247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -163,6 +163,7 @@ ActiveRecord::Schema.define(version: 20131011201504) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "bio"
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
