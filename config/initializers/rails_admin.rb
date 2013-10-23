b89d4aea936fc8cc038ba520d6c381a1bd51ae94
Elliot Murphy <elliot.murphy@gmail.com>
2013-10-23 01:43:19 -0400

    Tweaking blog permissions

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,7 @@
 RailsAdmin.config do |config|
   config.current_user_method { current_user }
   config.main_app_name = ["Stowaway", "BackOffice"]
-  config.included_models = ["Space", "User"]
+  config.included_models = ["Space", "User", "BlogitPost"]
   config.authorize_with do
     redirect_to root_path unless current_user.has_role? :admin
   end
