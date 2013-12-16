58b32b5904a1e8b824187a69ce9ff954db976106
Elliot Murphy <elliot.murphy@gmail.com>
2013-12-15 19:16:30 -0500

    Fixing failing tests for admin controller.

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
+# module StoreFrontend
+#   describe HomeController do
+
+#     describe "GET 'index'" do
+#       it "returns http success" do
+#         get 'index', use_route: 'store_frontend' # in my case
+#         response.should be_success
+#       end
+# module RailsAdmin
+
 describe AdminController do
 
   describe "GET 'index'" do
     it "not logged in user is rejected" do
-      get 'index'
+      get 'index', use_route: 'rails_admin'
       response.code.should == "302"
       response.should redirect_to(root_path)
     end
@@ -16,7 +26,7 @@ describe AdminController do
       end
 
       it "get index should succeed" do
-        get 'index'
+        get 'index', use_route: 'rails_admin'
         response.should be_success
       end
     end
