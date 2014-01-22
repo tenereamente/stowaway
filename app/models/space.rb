515cb8f556a54de859b4c6233c8c0aeb7bfa3fcd
Elliot Murphy <elliot.murphy@gmail.com>
2014-01-22 13:30:10 -0500

    Add model validations to prevent incomplete space listing crashes.

--- app/models/space.rb
+++ app/models/space.rb
@@ -14,24 +14,25 @@ class Space < ActiveRecord::Base
 
   has_many :space_rentals
   has_many :renters, :source => :user, :through => :space_rentals
+  # validations
+  validates :title, presence: true
+  validates :user_id, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true # later make this conditional based on country
+  validates :zip, presence: true # later make this conditional based on country.
+  validates :notes, presence: true
+  # TODO: catch Gmaps4rails address validation failure exception,
+  # currently it shows up as ActiveRecord::RecordInvalid exception when running validations
+  # "Gmaps4rails address Sorry, not even Google could figure out where that is:"
 
-
+  # scopes
   scope :owned, -> { where('user_id is not null')}
   scope :available, -> { where('available is true')}
   scope :complete, -> { where('complete is true')}
-  # TODO scopes for all features, environment types
-
-  scope :by_user, lambda { |id|
-    where(:user_id => id)
-  }
-
-  scope :by_user_indoor, lambda { |id| 
-    where(:user_id => id, :environment => "indoor")
-  }
-
-  scope :by_user_outdoor, lambda { |id| 
-    where(:user_id => id, :environment => "outdoor")
-  }
+  scope :by_user, lambda { |id| where(:user_id => id) }
+  scope :by_user_indoor, lambda { |id| where(:user_id => id, :environment => "indoor") }
+  scope :by_user_outdoor, lambda { |id| where(:user_id => id, :environment => "outdoor") }
 
   def gmaps4rails_address
     "#{self.address1}, #{self.city}, #{self.state} #{self.zip}"
